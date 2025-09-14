0b7d98e878f8ee2f9eda368bd84c64a84ae55e6e
anjanimaurya <maurya.anjani@gmail.com>
2025-09-14 14:00:35 -0400

    Delete folderscan/target/release/deps directory
    deps dir deleted

--- folderscan/target/release/deps/cfg_if-f0529e133a8d43d7.d
+++
@@ -1,7 +0,0 @@
-/workspaces/CodeStore/Rust/folderscan/target/release/deps/cfg_if-f0529e133a8d43d7.rmeta: /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/workspaces/CodeStore/Rust/folderscan/target/release/deps/libcfg_if-f0529e133a8d43d7.rlib: /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/workspaces/CodeStore/Rust/folderscan/target/release/deps/cfg_if-f0529e133a8d43d7.d: /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs:
--- folderscan/target/release/deps/num_cpus-1e04d29d9e0f927a.d
+++
@@ -1,8 +0,0 @@
-/workspaces/CodeStore/Rust/folderscan/target/release/deps/num_cpus-1e04d29d9e0f927a.rmeta: /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/num_cpus-1.15.0/src/lib.rs /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/num_cpus-1.15.0/src/linux.rs
-
-/workspaces/CodeStore/Rust/folderscan/target/release/deps/libnum_cpus-1e04d29d9e0f927a.rlib: /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/num_cpus-1.15.0/src/lib.rs /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/num_cpus-1.15.0/src/linux.rs
-
-/workspaces/CodeStore/Rust/folderscan/target/release/deps/num_cpus-1e04d29d9e0f927a.d: /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/num_cpus-1.15.0/src/lib.rs /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/num_cpus-1.15.0/src/linux.rs
-
-/usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/num_cpus-1.15.0/src/lib.rs:
-/usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/num_cpus-1.15.0/src/linux.rs:
